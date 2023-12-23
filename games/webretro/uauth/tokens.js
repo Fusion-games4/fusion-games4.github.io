ce4ab5076dba180052cffda4b5202eae1148400f
Extraskilled56 <145171712+Extraskilled56@users.noreply.github.com>
2023-12-23 17:12:38 -0600

    Deleted google api tokens

--- games/webretro/uauth/tokens.js
+++ games/webretro/uauth/tokens.js
@@ -1,8 +1,3 @@
-// Google Drive
-var googleProjectId = "hallowed-digit-311307";
-var googleApiKey = "AIzaSyDuKbs32MBOZw87EJi0dsPVNHx6D4_QTOQ";
-var googleOauthClientId = "478810195063-c83gm38hf14o08gg4oqggql7pp9dqbdl.apps.googleusercontent.com";
-// scope: https://www.googleapis.com/auth/drive.readonly
 
 // Dropbox
 var dropboxAppKey = "2556z2wtdog5rop";
